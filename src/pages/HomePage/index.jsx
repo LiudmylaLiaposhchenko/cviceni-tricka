a357a594e533cd304f8ce57ab7ef3f1de3dcb0d8
LL <liaposhchenkol@dmail.com>
2023-12-26 19:27:16 +0100

    Add data binding

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,17 +1,26 @@
+import { useState } from 'react';
 import { TShirt } from '../components/TShirt';
 import { TShirtSetup } from '../components/TShirtSetup';
 import './style.css';
 
 export const HomePage = () => {
+  const [color, setColor] = useState('');
+  const [print, setPrint] = useState('');
+  const [type, setType] = useState('');
+
   return (
     <div className="container">
       <div className="tshirt-panel">
         <div className="tshirt-panel__tshirt">
-          <TShirt type="normalShortSleeve" color="yellow" print="Ahoj světe" />
+          <TShirt type={type} color={color} print={print} />
         </div>
         <div className="tshirt-panel__setup">
           <h2>Možnosti:</h2>
-          <TShirtSetup />
+          <TShirtSetup
+            onSelectColor={setColor}
+            onSelectPrint={setPrint}
+            onSelectType={setType}
+          />
         </div>
       </div>
     </div>
